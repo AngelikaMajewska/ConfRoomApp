8ddd62940f0023e0308eaf5ad8754393d35992fa
Angelika Majewska <143930253+AngelikaMajewska@users.noreply.github.com>
2025-01-23 00:06:50 +0100

    Delete ConfRoomApp directory

--- ConfRoomApp/project/confroom_app/admin.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-
-# Register your models here.
-from confroom_app.models import Room, Booking
-
-# dodaj te kategorie w panelu admina
-admin.site.register(Room)
-admin.site.register(Booking)
